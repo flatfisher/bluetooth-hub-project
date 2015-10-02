3207b83e1dbf465261197959e83ecbdee1be94be
Kyohei Tsuda <shimano.entou@gmail.com>
2015-10-02 14:40:14 -0700

    add Constants class.

---
+++ app/src/main/java/com/liferay/healthcareproject/Constants.java
@@ -0,0 +1,11 @@
+package com.liferay.healthcareproject;
+
+/**
+ * Created by flatfisher on 10/2/15.
+ */
+public class Constants {
+
+    public final static String CHARACTERISTIC_DIALOG = "characteristic_dialog";
+
+
+}
