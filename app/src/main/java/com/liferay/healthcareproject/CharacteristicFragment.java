252d616bb778e98573bb4a102a0cc16e7fbe2c60
Kyohei Tsuda <shimano.entou@gmail.com>
2015-10-02 17:16:45 -0700

    set up layout for characteristic dialog.

--- app/src/main/java/com/liferay/healthcareproject/CharacteristicFragment.java
+++ app/src/main/java/com/liferay/healthcareproject/CharacteristicFragment.java
@@ -1,12 +1,16 @@
 package com.liferay.healthcareproject;
 
 import android.app.Dialog;
-import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.AlertDialog;
-import android.view.LayoutInflater;
-import android.view.View;
+import android.util.DisplayMetrics;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by flatfisher on 9/25/15.
@@ -14,23 +18,87 @@ import android.view.View;
 
 public class CharacteristicFragment extends DialogFragment {
 
+    private TextView uuidNameText;
+
+    private TextView readResultText;
+
+    private EditText writeEditText;
+
+    private TextView notifyResultText;
+
+    private Button readButton;
+
+    private Button writeButton;
+
+    private Button notifyButton;
+
+    private TextView readProperty;
+
+    private TextView writeProperty;
+
+    private TextView notifyProperty;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        Dialog dialog = new Dialog(getActivity());
 
-        LayoutInflater inflater = (LayoutInflater)getActivity()
-                                         .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
 
-        View view = inflater.inflate(R.layout.fragment_characteristic, null);
+        dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+        dialog.setContentView(R.layout.fragment_characteristic);
 
-        builder.setView(view);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
-        return builder.create();
+        setViewById(dialog);
+
+        return dialog;
 
     }
 
-    private void finish(){
-        getFragmentManager().beginTransaction().remove(this).commit();
+    private void setViewById(Dialog dialog){
+
+        uuidNameText = (TextView)dialog.findViewById(R.id.uuid_name_text);
+
+        readResultText = (TextView)dialog.findViewById(R.id.read_text);
+
+        writeEditText = (EditText)dialog.findViewById(R.id.writ_edit);
+
+        notifyResultText = (TextView)dialog.findViewById(R.id.notify_text);
+
+        readButton = (Button)dialog.findViewById(R.id.read_button);
+
+        writeButton = (Button)dialog.findViewById(R.id.write_button);
+
+        readProperty = (TextView)dialog.findViewById(R.id.property_read);
+
+        writeProperty = (TextView)dialog.findViewById(R.id.property_write);
+
+        notifyProperty = (TextView)dialog.findViewById(R.id.property_notify);
+
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Dialog dialog = getDialog();
+
+        WindowManager.LayoutParams layoutParams = dialog.getWindow().getAttributes();
+
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+
+        int dialogWidth = (int) (metrics.widthPixels * 0.8);
+
+        int dialogHeight = (int) (metrics.heightPixels * 0.8);
+
+        layoutParams.width = dialogWidth;
+
+        layoutParams.height = dialogHeight;
+
+        dialog.getWindow().setAttributes(layoutParams);
+
+    }
+    
 }
